2ac18d7fd01da8a6cae5b0cdc14989dc59f9718b
tkravits <taylor.kravits@gmail.com>
2020-11-09 22:21:51 -0700

    added json access

--- API_Pull.py
+++ API_Pull.py
@@ -1,6 +1,7 @@
 import requests
 import urllib3
 import pandas as pd
+import json
 
 # sets the display so that when the code prints, it is readable
 pd.set_option('display.max_rows', 3000)
@@ -10,3 +11,14 @@ pd.set_option('display.width', 3000)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 auth_url = "https://api.parkwhiz.com/v4/oauth/token"
+
+params = open('params.txt', 'r')
+# sending get request and saving the response as response object
+r = requests.post(url=auth_url, params=params.read())
+
+print("Requesting Token...\n")
+
+access_token = r.json()['access_token']
+print("Access Token = {}\n".format(access_token))
+
+header = {'Authorization': 'Bearer ' + access_token}
