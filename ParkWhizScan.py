b953ad0a26010e1891887082efe2328bcce990e2
tkravits <taylor.kravits@gmail.com>
2020-11-27 20:18:46 -0700

    Added some comments and first attempt at getting the code to run in a loop

--- ParkWhizScan.py
+++ ParkWhizScan.py
@@ -3,9 +3,11 @@ import urllib3
 import pandas as pd
 import json
 import numpy as np
+import time
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+# This is a list of urls that we'll utilize using the requests library
 auth_url = "https://api.parkwhiz.com/v4/oauth/token"
 booking_url = "https://api.parkwhiz.com/v4/bookings/?final_price=0.0"
 Eldora_event_url = "https://api.parkwhiz.com/v4/venues/478490/events/?pretty=True&fields=event::default,event:availability,site_url"
@@ -13,8 +15,10 @@ Copper_event_url = "https://api.parkwhiz.com/v4/venues/448854/events/?pretty=Tru
 quote_url = "https://api.parkwhiz.com/v4/quotes/"
 leftover_url = "&capabilities=capture_plate:always&option_types=bookable%20non_bookable%20on_demand&envelope=True"
 
+# Login info
 params = open('params.txt', 'r')
 
+# Create a df of my input dates
 df_dates = pd.read_json('dates.json').rename(columns={'dates': 'name'})
 
 # sending get request and saving the response as response object
@@ -25,13 +29,12 @@ print("Requesting Token...\n")
 access_token = r.json()['access_token']
 print("Access Token = {}\n".format(access_token))
 
+# Setting up the headers
 headers = {'Authorization': 'Bearer ' + access_token}
 
 
-def getAllEventIds():
+def getAllEventIds(df):
     # Pull list of all parking from Eldora and grab the unique event_id
-    col_names = ['name', 'id']
-    events = pd.DataFrame(columns=col_names)
     page = 1
     while True:
 
@@ -45,20 +48,19 @@ def getAllEventIds():
 
         # otherwise add new data to dataframe
         for x in range(len(b)):
-            events.loc[x + (page - 1) * 50, 'id'] = b[x]['id']
-            events.loc[x + (page - 1) * 50, 'name'] = b[x]['name']
+            df.loc[x + (page - 1) * 50, 'id'] = b[x]['id']
+            df.loc[x + (page - 1) * 50, 'name'] = b[x]['name']
         # increment page
         page += 1
 
-    parking = pd.DataFrame(events)
-    return parking
+    return df
 
 
 # Create a df of all parking for Eldora
-event_df = getAllEventIds()
+event_df = getAllEventIds(df)
 event_df['name'] += ' ' + event_df.groupby('name').cumcount().map({0: '9am', 1: '11am', 2: '1:30pm', 3: '2:30pm'})
 
-
+# Merge the dates I want with the Eldora's event ids
 def createDesiredParkingDataFrame():
     df_parking_dates = pd.merge(df_dates, event_df, on='name')
     df_parking_dates['Book_Status'] = False
@@ -69,32 +71,33 @@ df_desired_parking = createDesiredParkingDataFrame()
 
 def getAvailability():
     y = 0
-    for i in range(len(df_desired_parking.index)): # Need to get a max count from df to loop so I don't have to manually put a number in
+    for i in range(len(df.index)):
         # Create a url to get a quote id from a list of bookable event ids
-        quote = requests.get(quote_url + '?q=event_id:' + str(df_desired_parking['id'][y]) + leftover_url)
+        quote = requests.get(quote_url + '?q=event_id:' + str(df['id'][y]) + leftover_url)
         q = quote.json()
-        print(df_desired_parking['id'][y])
+        print(df['id'][y])
 
         if q == []: # Equivalent to booked parking = False
-            df_desired_parking.loc[y, ['Book_Status']] = False # This works so far
+            df.loc[y, ['Book_Status']] = False # This works so far
 
         if quote.ok:
             # If event is able to be booked add the quote id and the event id to the dataframe
             for z in range(len(q)):
-                df_desired_parking.loc[z + y, 'quote id'] = q[z]['purchase_options'][0]['id']
+                df.loc[z + y, 'quote id'] = q[z]['purchase_options'][0]['id']
                 print('id added')
-                df_desired_parking.loc[z + y, 'id'] = q[z]['_embedded']['pw:event']['id']
+                df.loc[z + y, 'id'] = q[z]['_embedded']['pw:event']['id']
                 #df_desired_parking.loc[z + y, 'Book_Status'] = True
 
         y += 1
-    return df_desired_parking
+    return df
 
 
 def BookEvent(df):
     y = 0
+    # Create a mask to make sure that the quote id has a bookable quote and that Book_Status is False
     mask = df['quote id'].notna() & ~df['Book_Status']
     df['book it'] = np.where(mask, True, False)
-    for i in range(len(df_desired_parking.index)):
+    for i in range(len(df.index)):
         if df['book it'][y] == True:
             # If quote id is successfully pulled, move on to book the event
                 booking = requests.post(
@@ -110,6 +113,18 @@ def BookEvent(df):
         y += 1
     return df
 
-df_booked_parking = getAvailability()
-BookEvent(df_booked_parking)
+getAvailability(df_booked_parking)
+#BookEvent(df_booked_parking)
 
+# Need to make a loop that will check to see if the count of Book_Status is greater than 7, wait
+# Then go back again and check to see if the parking is available again
+def check_seven_day_restrictions(df):
+    for i in range(len(df.index)):
+        if len(df['Book_Status'] == True) >= 7:
+            time.sleep(60)
+
+        else:
+            getAvailability(df)
+            BookEvent(df)
+
+check_seven_day_restrictions(df_booked_parking)
