f69706a69e739f38ccb2c2040679531beee465fc
tkravits <taylor.kravits@gmail.com>
2021-03-26 11:20:19 -0600

    Added license plate input, this is going to be published for everyone now that it's working properly

--- ParkWhizScan.py
+++ ParkWhizScan.py
@@ -4,6 +4,8 @@ import pandas as pd
 import json
 import numpy as np
 import time
+import sys
+from datetime import datetime
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -15,19 +17,33 @@ Copper_event_url = "https://api.parkwhiz.com/v4/venues/448854/events/?pretty=Tru
 quote_url = "https://api.parkwhiz.com/v4/quotes/"
 leftover_url = "&capabilities=capture_plate:always&option_types=bookable%20non_bookable%20on_demand&envelope=True"
 
+# datetime object containing current date and time
+now = datetime.now()
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
 # Login info
-params = open('params.txt', 'r')
+print('Please enter email')
+x = input()
+print('Please enter password')
+y = input()
+print('Please enter license plate')
+lice_plate = input()
+param_url = "grant_type=password&customer_email=" + x + "&customer_password=" + y +"&scope=public"
+params = param_url
 
 # Create a df of my input dates
 df_dates = pd.read_json('dates.json').rename(columns={'dates': 'name'})
 
 # sending get request and saving the response as response object
-r = requests.post(url=auth_url, params=params.read())
+r = requests.post(url=auth_url, params=params)
 
 print("Requesting Token...\n")
 
 access_token = r.json()['access_token']
-print("Access Token = {}\n".format(access_token))
+# using f-string to format the printing of the access_token
+print(f"Access Token = {access_token}\n")
 
 # Setting up the headers
 headers = {'Authorization': 'Bearer ' + access_token}
@@ -59,16 +75,27 @@ def getAllEventIds():
 
 # Create a df of all parking for Eldora
 event_df = getAllEventIds()
+# takes the dataframe (or dict) of event_df name column and self sets the grouped name fields to 9am, 11am, etc
 event_df['name'] += ' ' + event_df.groupby('name').cumcount().map({0: '9am', 1: '11am', 2: '1:30pm', 3: '2:30pm'})
+
 
 # Merge the dates I want with the Eldora's event ids
 def createDesiredParkingDataFrame():
-    df_parking_dates = pd.merge(df_dates, event_df, on='name')
-    df_parking_dates['Book_Status'] = False
+    while True:
+        try:
+            df_parking_dates = pd.merge(df_dates, event_df, on='name')
+            df_parking_dates['Book_Status'] = False
+            df_parking_dates.loc[:, 'quote id'] = np.NaN
+            break
+        except ValueError:
+            print('Please open up dates.json and input present or future dates')
+            sys.exit(1)
+
     return df_parking_dates
 
 
 df_desired_parking = createDesiredParkingDataFrame()
+
 
 def getAvailability(df):
     y = 0
@@ -86,7 +113,8 @@ def getAvailability(df):
             for z in range(len(q)):
                 df.loc[z + y, 'quote id'] = q[z]['purchase_options'][0]['id']
                 print('id added')
-                df.loc[z + y, 'id'] = q[z]['_embedded']['pw:event']['id']
+                # Need to put that [0] if in their json response there is a "[ text ]"
+                df.loc[z + y, 'id'] = q[z]['purchase_options'][0]['pricing_segments'][0]['event']['id']
 
         y += 1
     return df
@@ -101,7 +129,7 @@ def BookEvent(df):
         if df['book it'][y] == True:
             # If quote id is successfully pulled, move on to book the event
                 booking = requests.post(
-                    booking_url + '&quote_id=' + str(df['quote id'][y]) + '&plate_number=027zzz',
+                    booking_url + '&quote_id=' + str(df['quote id'][y]) + '&plate_number=' + lice_plate,
                     headers=headers)
                 b = booking.json()
             # If the booking goes through, add the booking id to a dataframe and remove the booking from the df that
@@ -113,19 +141,24 @@ def BookEvent(df):
         y += 1
     return df
 
-#getAvailability(df_desired_parking)
-#BookEvent(df_desired_parking)
 
 # Need to make a loop that will check to see if the count of Book_Status is greater than 7, wait
 # Then go back again and check to see if the parking is available again
 def check_seven_day_restrictions(df):
     while True:
         if df['Book_Status'].sum() >= 7:
-            time.sleep(60)
-
+            time.sleep(86.4)
+            now = datetime.now()
+            # dd/mm/YY H:M:S
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print('All Booked Up ', dt_string)
         if df['Book_Status'].sum() < 7:
             getAvailability(df)
             BookEvent(df)
-            time.sleep(60)
+            time.sleep(86.4)
+            now = datetime.now()
+            # dd/mm/YY H:M:S
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print('Looking Again ', dt_string)
 
 check_seven_day_restrictions(df_desired_parking)
